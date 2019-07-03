a75a6fcb293076e2df7326c51839fe5fa8ec0439
winterfell1425 <winterfell1425@gmail.com>
2019-07-03 11:25:55 +0200

    kata

---
+++ src/main/java/kata3/StringToNumber.java
@@ -0,0 +1,8 @@
+package kata3;
+
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        int myInteger = Integer.parseInt(str);
+        return myInteger;
+    }
+}
---
+++ src/test/java/kata3/NumberToStringTest.java
@@ -0,0 +1,13 @@
+package kata3;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class NumberToStringTest {
+    @Test
+    public void tests() {
+        assertEquals("67", NumberToString.numberToString(67));
+        assertEquals("123", NumberToString.numberToString(123));
+        assertEquals("999", NumberToString.numberToString(999));
+    }
+}
